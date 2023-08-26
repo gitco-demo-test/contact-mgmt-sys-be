cc97656656cd0ea9f19a13be191382813b9842d5
rp-20230816 <143084274+rp-20230816@users.noreply.github.com>
2023-08-26 13:33:15 +0800

    Update serializers.py

--- contacts/serializers.py
+++ contacts/serializers.py
@@ -6,9 +6,9 @@ from .models import Contact
 
 # create a ContactSerializer class
 class ContactSerializer(serializers.HyperlinkedModelSerializer):
-    url = serializers.HyperlinkedIdentityField(view_name='contacts:detail')
+    address = serializers.HyperlinkedIdentityField(view_name='contacts:detail')
 
     # specify the model and fields
     class Meta:
         model = Contact
-        fields = ('url', 'id', 'name', 'address', 'email', 'mobile')
+        fields = ('address', 'id', 'name', 'address', 'email', 'mobile')
