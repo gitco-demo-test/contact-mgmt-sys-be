1a17344db66eb5342f5f24a180ee82b71df57471
gitco-demo-test <142766648+gitco-demo-test@users.noreply.github.com>
2023-08-26 13:41:38 +0800

    Update `serializers.py`, use `link` instead of `url`

--- contacts/serializers.py
+++ contacts/serializers.py
@@ -6,9 +6,9 @@ from .models import Contact
 
 # create a ContactSerializer class
 class ContactSerializer(serializers.HyperlinkedModelSerializer):
-    url = serializers.HyperlinkedIdentityField(view_name='contacts:detail')
+    link = serializers.HyperlinkedIdentityField(view_name='contacts:detail')
 
     # specify the model and fields
     class Meta:
         model = Contact
-        fields = ('url', 'id', 'name', 'address', 'email', 'mobile')
+        fields = ('link', 'id', 'name', 'address', 'email', 'mobile')
